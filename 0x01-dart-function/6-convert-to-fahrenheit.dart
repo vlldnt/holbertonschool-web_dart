3845e1025eb478c7357023e9e525dfbdb78daee5
vlldnt <vieilledent.adrien@gmail.com>
2026-04-13 12:24:32 +0200

    feat: calculation form a set object str et int

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,5 +1,6 @@
 List<double> convertToF(List<double> temperaturesInC) {
-  return temperaturesInC.map((temp) {
+  List<double> fList = temperaturesInC.map((temp) {
     return double.parse(((temp * 9 / 5) + 32).toStringAsFixed(2));
   }).toList();
+  return fList;
 }
---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,32 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int resultA = 0;
+  int resultB = 0;
+  teamA.forEach((String key, int value) {
+    if (key == "Free Throw") {
+      resultA += value;
+    }
+    if (key == "2 pointers") {
+      resultA += value * 2;
+    }
+    if (key == "3 pointers") {
+      resultA += value * 3;
+    }
+  });
+  teamB.forEach((String key, int value) {
+    if (key == "Free Throw") {
+      resultB += value;
+    }
+    if (key == "2 pointers") {
+      resultB += value * 2;
+    }
+    if (key == "3 pointers") {
+      resultB += value * 3;
+    }
+  });
+
+  return resultB == resultB
+      ? 0
+      : resultA > resultB
+      ? 1
+      : 2;
+}
