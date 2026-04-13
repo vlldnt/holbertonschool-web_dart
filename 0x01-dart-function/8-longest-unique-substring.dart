575dea38adc86e62b0a3c607dbc45cf00d4dae2b
vlldnt <vieilledent.adrien@gmail.com>
2026-04-13 12:40:55 +0200

    refactor: optimize longest unique substring function for efficiency

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,9 +1,17 @@
 String longestUniqueSubstring(String str) {
-  String uniques = "";
-  for (int i = 0; i < str.length; i++) {
-    if (!uniques.contains(str[i])) {
-      uniques += str[i];
+  Map<String, int> seen = {};
+  String longest = "";
+  int left = 0;
+
+  for (int right = 0; right < str.length; right++) {
+    String char = str[right];
+    if (seen.containsKey(char) && seen[char]! >= left) {
+      left = seen[char]! + 1;
+    }
+    seen[char] = right;
+    if (right - left + 1 > longest.length) {
+      longest = str.substring(left, right + 1);
     }
   }
-  return uniques;
+  return longest;
 }
