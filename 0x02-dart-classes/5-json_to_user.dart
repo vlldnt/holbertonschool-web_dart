051729bd6c8a1cb9881368c55748827265926f75
vlldnt <vieilledent.adrien@gmail.com>
2026-04-13 16:16:51 +0200

    fix: correct spacing in User toString method

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -26,6 +26,6 @@ class User {
 
   @override
   String toString() {
-    return "User(id: $id, name: $name, age: $age, height: $height)";
+    return "User(id : $id, name: $name, age: $age, height: $height)";
   }
 }
