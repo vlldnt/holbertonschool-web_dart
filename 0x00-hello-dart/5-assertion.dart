90f5baeb23dbbd619330dcdb82439a490ba1429c
vlldnt <vieilledent.adrien@gmail.com>
2026-04-13 10:34:50 +0200

    refactor: simplify score validation using assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,7 +1,5 @@
 void main(List<String> args) {
-  if (int.parse(args[0]) >= 80) {
-    print("You Passed");
-  } else {
-    throw AssertionError("The score must be bigger or equal to 80");
-  }
+  int nb = int.parse(args[0]);
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print('You Passed');
 }
